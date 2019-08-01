e59f101968702ffbab71f8f55ddf4aa86423fda2
Diego Scifo <diegoscifo@gmail.com>
2019-08-01 07:21:27 -0300

    Agregar módulos y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 
-export const appRoutes: Routes = [
-    { path: '', component: PagesComponent, children: [
-        { path: 'dashboard', component: DashboardComponent },
-        { path: 'progress', component: ProgressComponent },
-        { path: 'graficas1', component: Graficas1Component },
-        { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-    ] },
+const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent }
